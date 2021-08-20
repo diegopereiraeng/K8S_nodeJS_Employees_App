caa708880c820a1b390a4ed92e7fe9f6711791d1
Diego Pereira <diegopereira@Diegos-MacBook-Pro.local>
2021-08-19 21:03:00 -0300

    New Employees Experience

--- EmployeeKoalaDB/public/scripts/employees.js
+++ EmployeeKoalaDB/public/scripts/employees.js
@@ -1,3 +1,5 @@
+selectedList=[];
+
 $(document).ready(function(){
 	// Activate tooltip
 	$('[data-toggle="tooltip"]').tooltip();
@@ -6,10 +8,14 @@ $(document).ready(function(){
 	var checkbox = $('table tbody input[type="checkbox"]');
 	$("#selectAll").click(function(){
 		if(this.checked){
+            console.log("All Selected")
+            selectedList=[];
 			checkbox.each(function(){
-				this.checked = true;                        
+				this.checked = true; 
+                selectedList.push(this.value)                       
 			});
 		} else{
+            selectedList=[];
 			checkbox.each(function(){
 				this.checked = false;                        
 			});
@@ -17,7 +23,33 @@ $(document).ready(function(){
 	});
 	checkbox.click(function(){
 		if(!this.checked){
-			$("#selectAll").prop("checked", false);
+			$("#selectAll").prop("checked", false);          
 		}
+        if(this.checked){
+            console.log("Selected: "+this.value)
+            selectedList.push(this.value)
+		}
+        else{
+            for( var i = 0; i < selectedList.length; i++){ 
+                if ( selectedList[i] === this.value) { 
+                    console.log("Removed from list " + selectedList.splice(i, 1)); 
+                }
+            
+            }
+        }
 	});
+    $("#deleteEmployee").click(function(){
+        console.log("deleteEmployee")
+        $.each( selectedList, function( index, value ){
+            console.log("deleteEmployee: "+value)
+            $.post("./employee/:id/delete", {_id:value},function(data, status){
+                
+                console.log("Data: " + data + "\nStatus: " + status);
+            });
+        });
+    
+    });
 });
+
+
+
